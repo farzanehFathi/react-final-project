e496ee6efa848550de484495f3900973d87ad3fb
farzaneh <farzaneh.fth@gmail.com>
2023-07-16 16:47:22 +0200

    basic styling finished

--- src/Weather.js
+++ src/Weather.js
@@ -4,20 +4,44 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
-        <input type="text" placeholder="Please enter a city ..." />
-        <input type="submit" value="Search" />
+      <form className="mb-3">
+        <div className="row gx-0">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Please enter a city ..."
+              autoFocus="on"
+              className="form-control w-100 h-100"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="w-100 btn btn-primary"
+            />
+          </div>
+        </div>
       </form>
-      <h1>Paris</h1>
-      <ul>
-        <li>Sunday 13:31</li>
-        <li>Clear Sky</li>
-      </ul>
-      <div className="row">
-        <div className="col-6 ">
-          <div className="icon">icon</div>
+      <div className="location-summary mb-3">
+        {" "}
+        <h1>Paris</h1>
+        <ul>
+          <li>Sunday 13:31</li>
+          <li>Clear Sky</li>
+        </ul>
+      </div>
+
+      <div className="row gx-0">
+        <div className="col-6 weather-summary">
+          <div className="icon">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="clear Sky"
+            />
+          </div>
           <div className="temp">6</div>
-          <div className="unit">C</div>
+          <div className="unit">°C</div>
         </div>
         <div className="col-6">
           <ul>
