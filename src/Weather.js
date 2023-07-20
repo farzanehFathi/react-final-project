86023e58c6c4261863ec30a199b4a3f75502e580
farzaneh <farzaneh.fth@gmail.com>
2023-07-20 14:42:08 +0200

    small edits

--- src/Weather.js
+++ src/Weather.js
@@ -3,14 +3,14 @@ import axios from "axios";
 
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather({ city }) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
       ready: true,
-      cityName: response.name,
+      cityName: response.data.name,
       temperature: Math.round(response.data.main.temp),
       description: response.data.weather[0].description,
       humidity: Math.round(response.data.main.humidity),
@@ -44,7 +44,7 @@ export default function Weather() {
         </form>
         <div className="location-summary text-capitalize  mb-3">
           {" "}
-          <h1>paris</h1>
+          <h1>{weatherData.cityName}</h1>
           <ul>
             <li>{weatherData.date}</li>
             <li>{weatherData.description}</li>
@@ -71,10 +71,10 @@ export default function Weather() {
   } else {
     let apiKey = "32e12816b7e874a17bd13105b642a985";
     let unit = "metric";
-    let city = "London";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    console.log(url);
     axios.get(url).then(handleResponse);
 
-    return "Loading ...";
+    return `Loading...`;
   }
 }
