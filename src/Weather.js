0684320a345740234b551dd016078c9010365074
farzaneh <farzaneh.fth@gmail.com>
2023-07-20 13:41:36 +0200

    finished basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,23 @@
 import React from "react";
+import axios from "axios";
+
 import "./Weather.css";
 
 export default function Weather() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "32e12816b7e874a17bd13105b642a985";
+  let unit = "metric";
+  let city = "London";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+
+  axios.get(url).then(handleResponse);
   return (
     <div className="Weather">
       <form className="mb-3">
-        <div className="row gx-0">
+        <div className="row">
           <div className="col-9">
             <input
               type="search"
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
 import reportWebVitals from "./reportWebVitals";
 
 import "./index.css";
