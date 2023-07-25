3d839bc21bd9b67cbf3bb2c2961ca96b3115564f
farzaneh <farzaneh.fth@gmail.com>
2023-07-25 19:15:55 +0330

    WeatherForecast component created

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 
 import WeatherInfo from "./WeatherInfo";
-
+import WeatherForecast from "./WeatherForecast";
 import "./Weather.css";
 
 export default function Weather({ defaultCity }) {
@@ -65,6 +65,7 @@ export default function Weather({ defaultCity }) {
         </form>
 
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
